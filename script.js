29185cc7d873a398919b9d4d2a46921076a49a3a
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-07-17 23:20:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function Person(name, age) {
 	this.age=age;
 }
 Person.prototype.greet=function(){
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`);
+	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old`);
 }
 
 function Employee(name, age, jobTitle) {
-	Person.call(name,age);
+	Person.call(this,name,age);
 	this.jobTitle=jobTitle;
 }
 Employee.prototype.jobGreet=function(){
