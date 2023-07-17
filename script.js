3248a43c9bc3382d6aec12a10824836c7b39dcf9
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-07-17 23:12:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name=name;
+	this.age=age;
+}
+Person.prototype.greet=function(){
+	console.log(`Hello, my name is ${name}, I am ${this.age} years old`);
+}
 
-function Employee(name, age, jobTitle) {}
-
+function Employee(name, age, jobTitle) {
+	super(name,age);
+	this.jobTitle=jobTitle;
+}
+Employee.prototype.jobGreet=function(){
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+}
 // Do not change code below this line
+Employee.prototype.__proto__ = Person.prototype;
 window.Person = Person;
 window.Employee = Employee;
